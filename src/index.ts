8d548c9a96ca401c403c0bb982164c61ed1c947e
Koty BLEU <bleukoty@gmail.com>
2023-05-25 00:08:49 +0000

    1.0.5 before test

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,28 @@
-// import { Command } from "commander";
-// import { createExpressApp } from "./actions/apps";
+#!/usr/bin/env node
+
+import { Command } from "commander";
+import { createExpressApp } from "./actions/apps";
 import { displayCLIMenu } from "./menu-prompt";
 
-// const program = new Command();
+const program = new Command();
 
-// program
-//   .name('kbl-cli')
-//   .description('CLI to create and manage express and java spring app.')
-//   .version('1.0.5');
+program
+  .name('kbl-cli')
+  .description('CLI to create and manage express and java spring app.')
+  .version('1.0.5');
 
-// program.command('create-express')
-//   .argument('<string>', 'Application name')
-//   .description('create an express app')
-//   .option('-p <string>','--port <string>', '3000')
-//   .action((appName, options) => {
-//     createExpressApp(appName, "node-express", parseInt(options.p));
-//   });
+program.command('create-express')
+  .argument('<string>', 'Application name')
+  .description('create an express app')
+  .option('-p <string>','--port <string>', '3000')
+  .action((appName, options) => {
+    createExpressApp(appName, "node-express", parseInt(options.p));
+  });
 
-// program
-//   .option('-m','--menu')
-//   .action(async () => {
-//     await displayCLIMenu();
-//   });
+program
+  .option('-m','--menu')
+  .action(async () => {
+    await displayCLIMenu();
+  });
 
-// program.parse();
-displayCLIMenu();
+program.parse();
